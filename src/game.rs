5a180006a4b30424a407c53662192706abb57241
trit <trit@quantumtrip.org>
2019-09-09 19:59:23 -0400

    Checking in again

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,19 @@
-use amethyst::prelude::*;
+use {
+	amethyst::{
+		assets::{PrefabData, ProgressCounter},
+		derive::PrefabData,
+		ecs::prelude::Entity,
+		error::Error,
+		prelude::*
+	},	
+    serde::{Deserialize, Serialize},
+};
+
+#[derive(Debug, Clone, Deserialize, PrefabData)]
+pub struct MyPrefabData {}
+
+#[derive(Eq, PartialOrd, PartialEq, Hash, Debug, Copy, Clone, Deserialize, Serialize)]
+pub enum AnimationId {}
 
 #[derive(Default)]
 pub struct GameState {
@@ -9,6 +24,6 @@ impl SimpleState for GameState {
 	fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
 		let StateData { world, .. } = data;
 
-		
+
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 mod game;
 
-use amethyst::{
-    core::transform::TransformBundle,
-    prelude::*,
-    renderer::{
-        plugins::{RenderFlat2D, RenderToWindow},
-        types::DefaultBackend,
-        RenderingBundle,
+use {
+    amethyst::{
+        animation::AnimationBundle,
+        assets::PrefabLoaderSystem,
+        core::transform::TransformBundle,
+        prelude::*,
+        renderer::{
+            plugins::{RenderFlat2D, RenderToWindow},
+            sprite::SpriteRender,
+            types::DefaultBackend,
+            RenderingBundle,
+        },
+        utils::application_root_dir,
     },
-    utils::application_root_dir,
 };
 
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
 
-    use crate::game::GameState;
+    use crate::game::*;
 
     let app_root = application_root_dir()?;
 
@@ -22,18 +27,25 @@ fn main() -> amethyst::Result<()> {
     let display_config_path = app_root.join("config/display.ron");
 
     let game_data = GameDataBuilder::default()
+        .with(
+            PrefabLoaderSystem::<MyPrefabData>::default(),
+            "scene_loader",
+            &[],
+        )
+        .with_bundle(AnimationBundle::<AnimationId, SpriteRender>::new(
+            "sprite_animation_control",
+            "sprite_sampler_interpolation",
+        ))?
         .with_bundle(
             RenderingBundle::<DefaultBackend>::new()
                 .with_plugin(
-                    RenderToWindow::from_config_path(display_config_path)
-                        .with_clear([0, 0, 0, 1]),
+                    RenderToWindow::from_config_path(display_config_path).with_clear([0, 0, 0, 1]),
                 )
                 .with_plugin(RenderFlat2D::default()),
         )?
         .with_bundle(TransformBundle::new())?;
 
     let mut game = Application::new(asset_dir, GameState::default(), game_data)?;
-    
     game.run();
 
     Ok(())
